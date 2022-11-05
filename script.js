4f49619552a84672035d7aaccf3d120d64b00cf3
Alexandr <alexander@burlachenko>
2022-11-06 01:54:45 +0200

    adds first logics and enemies source files

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+/**@type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-// setting correct scaling:
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 100;
+const enemiesArray = [];
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+    this.speed = Math.random() * 4 - 2;
+  }
+
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
+  }
+
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i += 1) {
+  enemiesArray.push(new Enemy());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+
+  requestAnimationFrame(animate);
+}
+animate();
