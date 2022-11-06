497de3cf256baa9e9b76389d338e3883d9687d3b
Alexandr <alexander@burlachenko>
2022-11-06 02:21:09 +0200

    adds first enemy sprite, its animation and random movement

--- script.js
+++ script.js
@@ -3,25 +3,48 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
-const numberOfEnemies = 100;
+const numberOfEnemies = 300;
 const enemiesArray = [];
+
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
+    this.image = new Image();
+    this.image.src = "./enemies/enemy1.png";
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
     this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spritHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spritHeight / 2.5;
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
 
   update() {
     this.x += this.speed;
     this.y += this.speed;
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
 
   draw() {
     ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spritHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -35,6 +58,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame += 1;
 
   requestAnimationFrame(animate);
 }
