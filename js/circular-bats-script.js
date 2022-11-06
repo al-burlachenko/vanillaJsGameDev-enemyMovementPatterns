fa6577e574ec54ec4fb3b15c61109afcbad9ec2a
Alexandr <alexander@burlachenko>
2022-11-06 16:41:50 +0200

    adds new enemy

--- js/circular-bats-script.js
+++ js/circular-bats-script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
-const numberOfEnemies = 20;
+const numberOfEnemies = 300;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -11,30 +11,30 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "./enemies/enemy4.png";
+    this.image.src = "./enemies/enemy3.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 213;
-    this.spritHeight = 213;
-    this.width = this.spriteWidth / 2;
-    this.height = this.spritHeight / 2;
+    this.spriteWidth = 218;
+    this.spritHeight = 177;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spritHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
-    this.newX = Math.random() * (canvas.width - this.width);
-    this.newY = Math.random() * (canvas.height - this.height);
+
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.interval = Math.floor(Math.random() * 200 + 50);
+    // this.angle = Math.random() * 2;
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 2 + 0.5;
+    this.curve = Math.random() * 200 + 50;
   }
   update() {
-    if (gameFrame % this.interval == 0) {
-      this.newX = Math.random() * (canvas.width - this.width);
-      this.newY = Math.random() * (canvas.height - this.height);
-    }
-    let dx = this.x - this.newX;
-    let dy = this.y - this.newY;
-
-    this.x -= dx / 70;
-    this.y -= dy / 70;
+    this.x =
+      (canvas.width / 2) * Math.sin((this.angle * Math.PI) / 90) +
+      (canvas.width / 2 - this.width / 2);
+    this.y =
+      (canvas.height / 2) * Math.cos((this.angle * Math.PI) / 200) +
+      (canvas.height / 2 - this.height / 2);
+    this.angle += this.angleSpeed;
     if (this.x + this.width < 0) this.x = canvas.width; // make endless moving
     //animate sprites
     if (gameFrame % this.flapSpeed === 0) {
